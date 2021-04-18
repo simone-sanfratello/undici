abed546d30335421dab0b6256f37ffea9e167a16
Simone Sanfratello <simone@braceslab.com>
2021-04-18 09:03:59 +0200

    wip

--- docs/api/Errors.md
+++ docs/api/Errors.md
@@ -17,6 +17,9 @@ const { errors } = require('undici')
 | `SocketError`                |  `UND_ERR_SOCKET`                          | there is an error with the socket.                    |
 | `NotSupportedError`          |  `UND_ERR_NOT_SUPPORTED`                   | encountered unsupported functionality.                |
 | `ResponseContentLength`      |  `UND_ERR_REQUEST_CONTENT_LENGTH_MISMATCH` | body does not match content-length header in request  |
-| `RequestContentLength`       |  `UND_ERR_RESPONSE_CONTENT_LENGTH_MISMATCH`| body does not match content-length header in response |
+| `RequestContentLength`       |  `UND_ERR_RESPONSE_CONTENT_LENGTH_MISMATCH`| body does not match content-length header in response * |
 | `InformationalError`         |  `UND_ERR_INFO`                            | expected error with reason                            |
 | `TrailerMismatchError`       |  `UND_ERR_TRAILER_MISMATCH`                | trailers did not match specification                  |
+
+\* The `UND_ERR_RESPONSE_CONTENT_LENGTH_MISMATCH` is returned when the received body is less than the value in the received header `Content-Length`.  
+If the server try to send a body larger than `Content-Length`, Undici closes the connection when the length is received and return an `UND_ERR_INFO`, to prevent a cache poisoning attack. 
--- test/util.js
+++ test/util.js
@@ -19,3 +19,84 @@ test('isStream', (t) => {
   const ee = new EventEmitter()
   t.notOk(util.isStream(ee))
 })
+
+test('mismatchContentLength', (t) => {
+  const cases = [
+    { name: 'no body size no content-length', input: [0, 0], output: false },
+    { name: 'body size but no content-length', input: [0, 0], output: false },
+    { name: 'content-length but no body size', input: [0, 0], output: false },
+    { name: 'body size equal to content-length (no zero)', input: [100, 100], output: false },
+    { name: 'body size not equal to content-length', input: [100, 99], output: true }
+  ]
+
+  t.plan(cases.length)
+
+  for (const case_ of cases) {
+    t.test(case_.name, (t) => {
+      t.plan(1)
+      t.equal(util.mismatchContentLength(...case_.input), case_.output)
+    })
+  }
+})
+
+test('extractMeaningfulHeaders', (t) => {
+  const cases = [
+    {
+      input: [
+        Buffer.from('Date'),
+        Buffer.from('Thu, 01 Jan 1970 00:00:01 GMT'),
+        Buffer.from('Connection'),
+        Buffer.from('keep-alive'),
+        Buffer.from('Keep-Alive'),
+        Buffer.from('timeout=5'),
+        Buffer.from('Content-Length'),
+        Buffer.from('2')
+      ],
+      output: {
+        keepAlive: Buffer.from('timeout=5'),
+        trailers: null,
+        contentLength: 2
+      }
+    },
+    {
+      input: [
+        Buffer.from('Trailer'),
+        Buffer.from('content-length')
+      ],
+      output: {
+        keepAlive: null,
+        trailers: Buffer.from('content-length'),
+        contentLength: 0
+      }
+    },
+    {
+      input: [
+        Buffer.from('Trailer'),
+        Buffer.from('content-length'),
+        Buffer.from('Date'),
+        Buffer.from('Sun, 18 Apr 2021 06:35:54 GMT'),
+        Buffer.from('Connection'),
+        Buffer.from('keep-alive'),
+        Buffer.from('Keep-Alive'),
+        Buffer.from('timeout=5'),
+        Buffer.from('Transfer-Encoding'),
+        Buffer.from('chunked')
+      ],
+      output: {
+        keepAlive: Buffer.from('timeout=5'),
+        trailers: Buffer.from('content-length'),
+        contentLength: 0
+      }
+    }
+  ]
+
+  t.plan(cases.length)
+
+  for (let i = 0; i < cases.length; i++) {
+    const case_ = cases[i]
+    t.test(`case #${i}`, (t) => {
+      t.plan(1)
+      t.same(util.extractMeaningfulHeaders(case_.input), case_.output)
+    })
+  }
+})
